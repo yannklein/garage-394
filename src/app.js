571b89bce50b6e9c24b2a5cfaaa5a6e5a0eac972
Yann Klein <yann.klein@me.com>
2020-05-07 11:47:55 +0900

    sidebar width changed to fit half screen mode

--- src/app.js
+++ src/app.js
@@ -1,24 +1,6 @@
-import { getCars, postCar, carInfo} from './car';
-
 // TODO: give a badass name to your garage
-const myBadAssGarage = "harry-hot-road";
+const myBadAssGarage = "";
 
 // DON'T CHANGE THIS LINE
 document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
 // //////////////////////
-
-
-getCars(myBadAssGarage);
-// take car(s) and put into the HTML
-
-// POST
-// select submit button and inputs
-const submit = document.querySelector("#submit-btn");
-
-// listen to click on submit button
-submit.addEventListener("click", (event) => {
-  event.preventDefault();
-  postCar(carInfo(), myBadAssGarage);
-});
-// fetch info and post request
-// refresh car list
